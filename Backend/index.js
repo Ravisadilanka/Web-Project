48b78b06c6ffbddde0e58bbe6ae66295f69289f3
Ravisa Dilanka <raveeshadilanka1999@gmail.com>
2024-01-13 22:54:57 +0530

    Initial commit

---
+++ Backend/index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+const port = 4000;
+
+// Enable CORS for all routes
+app.use(cors());
+
+// Middleware to parse JSON
+app.use(express.json());
+
+// MongoDB Connection
+const mongoURI = "mongodb+srv://Raveesha:raveesha@cluster0.cnc9ie5.mongodb.net/webproject";
+mongoose
+    .connect(mongoURI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => {
+        console.log("Connected to MongoDB");
+    })
+    .catch((error) => {
+        console.error("Error connecting to MongoDB:", error.message);
+    });
+
+// Routes
+app.get("/", (req, res) => {
+    res.send("Express App is Running");
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
