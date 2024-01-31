deb506148fd578d73dfd164c21e1f140ac06d08a
Ravisa Dilanka <raveeshadilanka1999@gmail.com>
2024-01-31 22:51:05 +0530

    Started creating backend for Admin page

--- Admin/src/App.jsx
+++ Admin/src/App.jsx
@@ -1,10 +1,13 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
+import Navbar from './Components/Navbar/Navbar'
+import Admin from './Pages/Admin/Admin'
 
 function App() {
   return (
     <div>
-      
+      <Navbar />
+      <Admin />
     </div>
   )
 }
