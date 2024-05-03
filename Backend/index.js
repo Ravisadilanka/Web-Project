711d51718f663ef6f790d18a5495c07a6cbd3cbe
Ravisa Dilanka <raveeshadilanka1999@gmail.com>
2024-05-03 11:36:47 +0530

    Complete backend and connect admin page

--- Backend/index.js
+++ Backend/index.js
@@ -220,7 +220,7 @@ app.get('/popularinwomen', async (req, res) => {
 
 //Creating middelware to fetch user
 const fetchUser = async (req, res, next) => {
-    const token = req.header(auth - token);
+    const token = req.header('auth-token');
     if (!token) {
         res.status(401).send({ errors: "Please authenticate using valid token" })
     }
